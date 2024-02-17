b48a83e42c3b212bcf778892e3577636acae3b8c
Tomer Yosef <Tomeryosef04@gmail.com>
2024-02-17 22:02:24 +0200

    <Feat> Fix The remove Button & Giving it some Styles

--- src/components/MealTable.js
+++ src/components/MealTable.js
@@ -6,14 +6,16 @@ const MealTable = ({ category, items, onEdit, onRemove }) => {
     <div className="mealTable">
       <h2>{category}</h2>
       <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            <span>{item.Name}</span> 
-            <span>{item.calorie} kcal</span> 
-            <button className="editButton" onClick={() => onEdit(item)}>Edit</button>
-            <button className="removeButton" onClick={() => onRemove(item.id)}>Remove</button>
-          </li>
-        ))}
+      {items.map((item, index) => (
+      <li key={index}>
+        <span>{item.Name}</span> 
+        <span>{item.calorie} kcal</span>
+        <div className="buttonContainer">
+         <button className="editButton" onClick={() => onEdit(item)}>Edit</button>
+         <button className="removeButton" onClick={() => onRemove(item.id)}>Remove</button>
+      </div>
+    </li>
+    ))}
       </ul>
       {items.length === 0 && <p>No items added for {category} yet.</p>}
     </div>
